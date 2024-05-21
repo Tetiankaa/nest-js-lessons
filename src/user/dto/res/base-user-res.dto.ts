90b08046db79e6de45f1e2c22d0efa16d4b5e3c5
Tetiankaa <tanyakorsun0208@gmail.com>
2024-05-21 19:24:45 +0200

    init commit

---
+++ src/user/dto/res/base-user-res.dto.ts
@@ -0,0 +1,11 @@
+export class BaseUserResDto {
+  public readonly id: string;
+
+  public readonly name: string;
+
+  public readonly email: string;
+
+  public readonly password: string;
+
+  public readonly avatar: string;
+}
